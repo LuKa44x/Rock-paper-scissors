e49e6f11047d25c53a3016909400b44f303cec05
LuKa44x <lucavicari44@gmail.com>
2025-03-14 18:07:07 +0100

    Created new function for play rounds
    Added playRound() function that calls the other two function for play the round correctly.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+let humanScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice() {
     let computerAnswer;
     random = Math.floor(Math.random() * 3 + 1);
-    console.log(random);
     if (random == 1) { computerAnswer = "rock";}
     else if (random == 2) { computerAnswer = "paper";}
     else{ computerAnswer = "scissors";}
@@ -15,5 +17,34 @@ function getHumanChoice(){
     
 }
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+function playRound(getHumanChoice, getComputerChoice){
+    if (getHumanChoice == getComputerChoice){
+        console.log( "It's a tie!");
+    }
+    else if (getHumanChoice == "rock" && getComputerChoice == "scissors"){
+        humanScore++;
+    }
+    else if (getHumanChoice == "rock" && getComputerChoice == "paper"){
+        computerScore++;
+    }
+    else if (getHumanChoice == "paper" && getComputerChoice == "rock"){
+        humanScore++;
+    }
+    else if (getHumanChoice == "paper" && getComputerChoice == "scissors"){
+        computerScore++;
+    }
+    else if (getHumanChoice == "scissors" && getComputerChoice == "rock"){
+        computerScore++;
+    }
+    else if (getHumanChoice == "scissors" && getComputerChoice == "paper"){
+        humanScore++;
+    }
+    console.log("Human score: " + humanScore + " Computer score: " + computerScore);
+}
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
