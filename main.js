80132ab96904226c1ed3321a8dbd3e41939acdb4
LuKa44x <lucavicari44@gmail.com>
2025-03-14 17:40:34 +0100

    Created function for the choice
    Created a function called getComputerChoice for obtain the rock-paper-scissor choice
    Used Math random method for receive a random number from 1 to 3 and associated
    1 to rock, 2 to paper and 3 to scissors
    Used some console.log to test if everything was fine.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+
+function getComputerChoice() {
+    let choice;
+    random = Math.floor(Math.random() * 3 + 1);
+    console.log(random);
+    if (random == 1) { choice = "rock";}
+    else if (random == 2) { choice = "paper";}
+    else{ choice = "scissors";}
+    return choice;
+}
+
+
+console.log(getComputerChoice());
