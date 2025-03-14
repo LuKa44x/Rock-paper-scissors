427be7de9b4bfd77090d796a1ab7a349a043f8e9
LuKa44x <lucavicari44@gmail.com>
2025-03-14 17:50:13 +0100

    Added getHumanChoice function
    Added function with user input for rock-paper-scissors
    Added a new console.log line for verify that everything works
    Changed the variable "choice" into computerAnswer for better understanding.

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 
 function getComputerChoice() {
-    let choice;
+    let computerAnswer;
     random = Math.floor(Math.random() * 3 + 1);
     console.log(random);
-    if (random == 1) { choice = "rock";}
-    else if (random == 2) { choice = "paper";}
-    else{ choice = "scissors";}
-    return choice;
+    if (random == 1) { computerAnswer = "rock";}
+    else if (random == 2) { computerAnswer = "paper";}
+    else{ computerAnswer = "scissors";}
+    return computerAnswer;
 }
 
+function getHumanChoice(){
+    let userAnswer = prompt ("Rock, paper or scissors?");
+    return userAnswer;
+    
+}
 
+console.log(getHumanChoice());
 console.log(getComputerChoice());
